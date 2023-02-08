77ba9129077327279f346638bab422f98a3e0db4
程果 <guo@hodokas-MacBook-Pro.local>
2023-02-07 21:28:27 -0500

    debug gelu

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -20,6 +20,6 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         64,
         output_dim,
         2,
-        torch.nn.functional.gelu(),
+        torch.nn.functional.gelu,
         torch.nn.init.xavier_uniform_,
     )
