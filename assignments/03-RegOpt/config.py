4a11a642c20e752b1ecbb5f0aeb89bcfa61318ed
程果 <guo@hodokas-MacBook-Pro.local>
2023-02-19 12:35:11 -0500

    hw3 time based decay lr scheduler - add docstring

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,14 +7,16 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 30
+    num_epochs = 10
     initial_learning_rate = 0.01
     initial_weight_decay = 0.001
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "num_epochs": num_epochs,
         "factor": 0.001,
+        "initial_learning_rate": initial_learning_rate,
     }
 
     optimizer_factory: Callable[
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -5,7 +5,13 @@ import numpy as np
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, factor=0.9, last_epoch=-1):
+    """
+    Learning rate Scheduler class that adjust learning rate
+    """
+
+    def __init__(
+        self, optimizer, num_epochs, initial_learning_rate, factor=0.9, last_epoch=-1
+    ):
         """
         Create a new scheduler.
 
@@ -15,9 +21,17 @@ class CustomLRScheduler(_LRScheduler):
         """
         # ... Your Code Here ...
         self.factor = factor
+        self.num_epochs = num_epochs
+        self.initial_learning_rate = initial_learning_rate
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        """
+        Arguments:
+        None
+        Returns:
+        List[float] the learning rate for this epoch
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
@@ -25,9 +39,18 @@ class CustomLRScheduler(_LRScheduler):
         # Here's our dumb baseline implementation:
         # print(type(self.last_epoch))
 
+        # print(self.last_epoch)
         if self.last_epoch == 0:
             return [i for i in self.base_lrs]
-        return [i * np.exp(-self.factor * self.last_epoch) for i in self.base_lrs]
+        # if self.last_epoch >= 2346:
+        #     self.factor *= 2**(1/782)
+
+        # this is time based decay
+        decay = self.initial_learning_rate / self.num_epochs
+        return [i * 1 / (1 + decay * self.last_epoch) for i in self.base_lrs]
+
+        # This is expo decay - current best
+        # return [i * np.exp(-self.factor * self.last_epoch) for i in self.base_lrs]
 
         # return [i*j for (i, j) in zip(self.base_lrs , decay)]
         # return [group["lr"] * self.factor for group in self.optimizer.param_groups]
