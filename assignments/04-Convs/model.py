f2595059a5944f0c40fa26d6a2745c38af45978f
程果 <guo@hodokas-MacBook-Pro.local>
2023-03-17 21:47:43 -0400

    2conv 2linear 2batchnorm 64lin dropout

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -24,14 +24,20 @@ class Model(torch.nn.Module):
         #     torch.nn.Linear(256,num_classes)
         # )
 
+        self.initializer = torch.nn.init.xavier_uniform_
+
         self.conv1 = torch.nn.Conv2d(
             num_channels, 16, kernel_size=3, stride=2, padding=1
         )
+        self.initializer(self.conv1.weight)
         self.batch1 = torch.nn.BatchNorm2d(16)
         self.conv2 = torch.nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
+        self.initializer(self.conv2.weight)
         self.batch2 = torch.nn.BatchNorm2d(16)
 
         self.fc1 = torch.nn.Linear(16 * 8 * 8, 64)
+        self.initializer(self.fc1.weight)
+        self.dropout = torch.nn.Dropout(0.5)
         self.fc2 = torch.nn.Linear(64, num_classes)
         self.relu = torch.nn.ReLU()
 
@@ -45,12 +51,12 @@ class Model(torch.nn.Module):
         x = self.batch1(x)
         x = self.relu(x)
         x = self.conv2(x)
-        x = self.batch2(x)
         x = self.relu(x)
 
         x = x.view(-1, 16 * 8 * 8)
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
 
         return x
